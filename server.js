55a34663ed1094bf155d21439704e6fbac2ecc18
gabrieldubiela <165738698+gabrieldubiela@users.noreply.github.com>
2025-07-14 13:58:31 -0300

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const utilities = require("./utilities/")
 
 /* ***********************
  * View Engine and Templates
@@ -26,6 +27,20 @@ app.use(static)
 // Index route
 app.get("/", function(req, res){
   res.render("index", {title: "Home"})
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
 })
 
 /* ***********************
