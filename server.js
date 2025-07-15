e4fbf021e786987915f342eb217546032bd2733e
gabrieldubiela <165738698+gabrieldubiela@users.noreply.github.com>
2025-07-15 14:43:52 -0300

    Update server.js

--- server.js
+++ server.js
@@ -26,9 +26,7 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 // Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
+app.get("/", baseController.buildHome)
 
 /* ***********************
 * Express Error Handler
