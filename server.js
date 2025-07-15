0bde8e7a21d6b157a7270708743c2c88f1d9de63
gabrieldubiela <165738698+gabrieldubiela@users.noreply.github.com>
2025-07-15 14:11:10 -0300

    Update server.js

--- server.js
+++ server.js
@@ -43,6 +43,11 @@ app.use(async (err, req, res, next) => {
   })
 })
 
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
