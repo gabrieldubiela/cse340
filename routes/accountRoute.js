cd170a7d38d2dd91106ba26a61db1dc5e8926caa
Gabriel Dubiela <gabriel.dubiela@hotmail.com>
2025-07-23 23:24:45 -0300

    leraning activities try

---
+++ routes/accountRoute.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = new express.Router();
+const accountController = require("../controllers/accountController");
+const utilities = require("../utilities");
+
+// Route to "My Account"
+router.get("/login", utilities.handleErrors(accountController.buildLogin));
+
+// Route to Register Page
+router.get("/register", utilities.handleErrors(accountController.buildRegister));
+
+// Route to submit registration form
+router.post('/register', utilities.handleErrors(accountController.registerAccount))
+
+module.exports = router;
--- server.js
+++ server.js
@@ -8,19 +8,51 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const env = require("dotenv").config();
+const bodyParser = require("body-parser");
 const app = express();
+const session = require("express-session");
+const pool = require("./database/");
+
 const static = require("./routes/static");
-const baseController = require("./controllers/baseController");
-const utilities = require("./utilities/");
+const accountRoute = require("./routes/accountRoute");
 const inventoryRoute = require("./routes/inventoryRoute");
+
+const utilities = require("./utilities/");
 const errorController = require("./controllers/errorController");
+const baseController = require("./controllers/baseController");
 
 /* ***********************
  * View Engine and Templates
  *************************/
 app.set("view engine", "ejs");
 app.use(expressLayouts);
-app.set("layout", "./layouts/layout"); // not at views root
+app.set("layout", "./layouts/layout");
+
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(
+  session({
+    store: new (require("connect-pg-simple")(session))({
+      createTableIfMissing: true,
+      pool,
+    }),
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    name: "sessionId",
+  })
+);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Express Messages Middleware
+app.use(require("connect-flash")());
+app.use(function (req, res, next) {
+  res.locals.messages = require("express-messages")(req, res);
+  next();
+});
 
 /* ***********************
  * Routes
@@ -32,6 +64,9 @@ app.get("/", utilities.handleErrors(baseController.buildHome));
 
 // Inventory routes
 app.use("/inv", inventoryRoute);
+
+// Account routes
+app.use("/account", accountRoute);
 
 // 500 Error Route
 app.get("/error", utilities.handleErrors(errorController.triggerError));
@@ -51,7 +86,8 @@ app.use(async (err, req, res, next) => {
   if (err.status == 404) {
     message = err.message;
   } else {
-    message = "Oops! It looks like something went wrong. But don't worry, even the best servers have their bad days.";
+    message =
+      "Oops! It looks like something went wrong. But don't worry, even the best servers have their bad days.";
   }
   res.render("errors/error", {
     title: err.status || "Server Error",
