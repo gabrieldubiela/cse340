fe84b973102a91cbf4d9d8a8dfb91ea444b02131
Gabriel Dubiela <gabriel.dubiela@hotmail.com>
2025-07-30 10:14:36 -0300

    fix error

--- controllers/invController.js
+++ controllers/invController.js
@@ -30,7 +30,7 @@ invCont.buildByInvId = async function (req, res, next) {
   const data = await invModel.getInventoryById(inv_id);
   let nav = await utilities.getNav();
 
-  if (!data || data.length < 1) {
+  if (!data) { 
     return next({
       status: 404,
       message: "Sorry, we could not find that vehicle.",
@@ -38,10 +38,10 @@ invCont.buildByInvId = async function (req, res, next) {
   }
 
   // Build the HTML for the vehicle details
-  const vehicleDetails = await utilities.buildVehicleDetail(data[0]);
+  const vehicleDetails = await utilities.buildVehicleDetail(data);
 
   // Set the title using the vehicle make and model
-  const title = `${data[0].inv_make} ${data[0].inv_model}`;
+  const title = `${data.inv_make} ${data.inv_model}`;
 
   res.render("./inventory/detail", {
     title: title,
